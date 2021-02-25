1bd2fac4ffc65fbfadc5bb5f2ee237190e39a673
sara gharai <arshiagh1998@gmail.com>
2021-02-25 17:46:48 -0500

    made some changes

---
+++ team27/src/profile.js
@@ -0,0 +1,15 @@
+import './profile.css';
+import React, { Profiler } from 'react';
+
+class profiler extends React.Component{
+	render(){
+		return(
+			<div>
+				<h1>Profile</h1> 
+			</div>
+		
+		)
+	}
+}
+
+export default profiler;
