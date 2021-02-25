44f0317e551ab1bb747e8e18afe938e0e54925f1
arshia gharai <36425226+arshiagh1998@users.noreply.github.com>
2021-02-24 19:55:08 -0500

    update

--- team27/src/App.js
+++ team27/src/App.js
@@ -5,8 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import AppBar from '@material-ui/core/AppBar'
 import buttonGroup from '@material-ui/core/ButtonGroup'
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { createMuiTheme, styled } from '@material-ui/core/styles';
-import { spacing } from '@material-ui/system'
+import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 import Image from 'material-ui-image'
@@ -18,11 +17,16 @@ import {
   ListItemText
 } from '@material-ui/core' 
 import Typography from '@material-ui/core/Typography'
+import resume from './resume.png'
+import TextField from '@material-ui/core/TextField';
+import { palette, spacing } from '@material-ui/system';
+
+
+
 
 
 const MyButton = styled(Button)({
   marginRight: 50,
- 
   color: "white",
 });
 
@@ -40,74 +44,64 @@ const MyButton3 = styled(Button)({
   
 });
 
-const useStyles = makeStyles({
-  
-  drawer: {
-      backgroundColor: "black",
-      width: 500,
-      marginTop: 500
-}});
-
-
 
 
 const Drawer = () =>{
-  const classes = useStyles();
   return(
-      <MUIDrawer variant="permanent" className={classes.drawer} >
-    <List>
-      {["overview         ", "Users", "Posts"].map((text, index) => (
-      <ListItem button key={text}>
-        <ListItemText primary={text}/>
-      </ListItem>
-      ))}
-    </List>
-  </MUIDrawer>
+      <MUIDrawer variant="permanent">
+        <List style={{ backgroundColor:'#71A89E', top: 65, color: 'white' }}>
+          {["overview", "Users", "Posts"].map((text, index) => (
+          <ListItem button key={text}>
+             <ListItemText primary={text}/>
+          </ListItem>
+          ))}
+        </List>
+      </MUIDrawer>
 );
 };
 
-const typo = styled(Typography)({
-  position: "relative",
-  marginTop : 500,
-  marginLeft : 500
-});
 
 
 class App extends React.Component {
-
   render(){
     return(
       <div>
         
 
-        <AppBar>
-        <Toolbar>
-          <MyButton2
-          variant="outlined">
-            Admin
-          </MyButton2 >
+        <AppBar style={{ backgroundColor:'#71A89E'}}>
+          <Toolbar  >
+          <img className="landingPic" src={resume} />
+            <MyButton2 variant="outlined">
+              Admin
+            </MyButton2 >
 
-          <MyButton variant="outlined">
-            Explore
-          </MyButton>
+            <MyButton variant="outlined">
+              Explore
+            </MyButton>
 
-          <MyButton variant="outlined">
-            Help
-          </MyButton>
+            <MyButton variant="outlined">
+              Help
+            </MyButton>
 
-          <MyButton3 variant="outlined">
-            Log out
-          </MyButton3>
-        </Toolbar>
+            <MyButton3 variant="outlined">
+              Log out
+            </MyButton3>
+           
+          </Toolbar>
         </AppBar>
 
+        <h1>Overview</h1>
+        <div >
+        <Typography className='button'>
+          sadas
+        </Typography>
+        </div>
+        
 
-        <Drawer/>
 
-        <typo>
-          hi
-        </typo>
+      
 
+        
 
 
         
